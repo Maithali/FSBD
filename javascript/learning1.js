49c4807bd3818cab916a126af0c920917519138b
Maithali <ghardem1@gmail.com>
2024-07-02 20:13:16 +0530

    javascript learing

--- javascript/learning1.js
+++ javascript/learning1.js
@@ -10,3 +10,19 @@ const mobileno = 9878564823;
 
 console.log("my firstname is" + firstname);
 console.log('my lastname is',lastname);
+
+//template literal
+console.log(`my current first name is 
+    ${firstname}
+    ${lastname}
+    ${mobileno}`);
+
+//assignment operator
+let x1 = 11;
+let y = 10;
+
+// arthimatic Operator
+// +,-,*,/,%
+let z1 = x1 + y;
+console.log(z1);
+console.log(x1 + y);
