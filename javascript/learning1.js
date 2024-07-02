6ae11e4e961179bb71bcc01a3ce76cb71c84d360
Maithali <ghardem1@gmail.com>
2024-07-02 20:04:28 +0530

    javascript learing

---
+++ javascript/learning1.js
@@ -0,0 +1,12 @@
+//Variables => place holder for value
+
+let x =25;
+const z = '5989254';
+const balance = 9999999;
+
+let firstname ='Maithali';
+let lastname = "Tiwari";
+const mobileno = 9878564823;
+
+console.log("my firstname is" + firstname);
+console.log('my lastname is',lastname);
