ac3ea46aaea7244925904702cd76bbeed5d80eb5
Maithali <ghardem1@gmail.com>
2024-07-03 17:14:53 +0530

    javascript conditions

---
+++ javascript/learning2.js
@@ -0,0 +1,12 @@
+    // Conditions
+    // Rating mini App
+    let rating = 5;
+    if (rating == 5) {
+        console.log('5 Stars');
+    }else if (rating ==4) {
+        console.log('4 stars');
+    }else if (rating == 3) {
+        console.log('3 Stars');
+    }else{
+        console.log('i am else');
+    }
