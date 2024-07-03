d9dbdc573995e984b799864192b927e6274ad73f
Maithali <ghardem1@gmail.com>
2024-07-03 17:25:19 +0530

    javascript conditions

--- javascript/learning2.js
+++ javascript/learning2.js
@@ -10,3 +10,23 @@
     }else{
         console.log('i am else');
     }
+
+    // Switch Case
+    let user = 'Admin';
+
+    switch (user) {
+        case 'Admin':
+            console.log('redirect him/Her to the Admin Page');
+            break;
+        case 'Mentor':
+            console.log('Redirect him/her to Mentor page');
+            break;
+        default:
+            console.log('Simple user');
+            break;
+    }
+
+    // Ternary Opertor / Ternanry Condition
+    // Condition ? True : Flase
+    let userlogin = false;
+    userlogin ? console.log('logout') : console.log('login');;
