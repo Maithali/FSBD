0ce1eb25c389846c9e012fbc8fe702f7d7c0acf6
Maithali <ghardem1@gmail.com>
2024-07-03 12:06:45 +0530

    learing solidity

---
+++ solidity/ExampleContract.sol
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+    contract ExampleContract {
+
+        function helloWorld(uint256 input) public pure returns (uint256) {
+            return input;
+        }
+
+        function haloDunia(bool answer) public pure returns  (bool) {
+            return answer;
+        }
+        
+    }
