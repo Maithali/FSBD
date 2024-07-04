064a8a80e75264823e27b29dcbb9899dc9955832
Maithali <ghardem1@gmail.com>
2024-07-04 09:57:58 +0530

    practice problem

--- solidity/MultipleofTen.sol
+++ solidity/MultipleofTen.sol
@@ -1,13 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma  solidity ^0.8.13;
+pragma solidity ^0.8.13;
 
-    contract ExampleIfStatement {
-        function isAMultipleOfTen(uint256 x) public  pure  returns (bool) {
-            if (x % 10 == 0) {
-                return  true;
-            }
-            else {
-                return false;
-            }
+// contract ExampleIfStatement {
+//     function isAMultipleOfTen(uint256 x) public  pure  returns (bool) {
+//         if (x % 10 == 0) {
+//             return  true;
+//         }
+//         else {
+//             return false;
+//         }
+//     }
+// }
+
+contract ExampleIfStatement {
+    function isAMultipleOfTen(uint256 x) public pure returns (bool) {
+        bool isMul = x % 10 == 0;
+        if (isMul) {
+            return true;
+        } else {
+            return false;
         }
     }
+}
