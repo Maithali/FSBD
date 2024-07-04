232b7e574823acacf485230d1a1f5222e75f1177
Maithali <ghardem1@gmail.com>
2024-07-04 18:20:32 +0530

    for loop

---
+++ javascript/array.js
@@ -0,0 +1,18 @@
+/*
+    => Data Types
+    =>Variables and typeof
+    => Date
+    => Loops
+    => Conditions
+    => OP
+    => Indexing
+    => Math
+    => Object and Array
+    */
+
+    let namess = new Array('Name 1','Name 2');
+    console.log(namess);//[ 'Name 1', 'Name 2' ]
+
+    // Push => Insert New Values inside the Array
+    namess.push("Maithali");
+    console.log(namess);//[ 'Name 1', 'Name 2', 'Maithali' ]
