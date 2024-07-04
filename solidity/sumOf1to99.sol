39bbb2c44d11a7eca0e3e03749ce2400ccda231b
Maithali <ghardem1@gmail.com>
2024-07-04 10:28:20 +0530

    for loop

---
+++ solidity/sumOf1to99.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract SumOFAll {
+
+    function sum() public  pure returns (uint256) {
+    //    . Here is the code to add up all the numbers from 1 to 99
+    uint256 addup = 0;
+
+    for(uint256 i = 0; i < 100; i++){
+        addup + = i;
+    }
+    return addup;
+
+
+    }
+    
+}
