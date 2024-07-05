0073d9c42d328ad8452266d3e42723e64a15bc1c
Maithali <ghardem1@gmail.com>
2024-07-05 20:27:23 +0530

    string solidity

--- solidity/String.sol
+++ solidity/String.sol
@@ -2,22 +2,6 @@
 pragma solidity ^0.8.13;
 
 contract Example {
-    //     function AddressArray(address[] calldata input)public  pure returns (address[] memory) {
-    //         return input;
-    //     }
-
-    //     function StringArray(string[] calldata name) public  pure returns(string[] memory) {
-    //         return name;
-    //     }
-
-    //     function BooleanArray(bool[] calldata value) public  pure  returns (bool[] memory) {
-    //         return value;
-    //     }
-
-    //   function IntergerArray(uint256[] calldata no)public pure  returns (uint256[] memory) {
-    //       return  no;
-    //     }
-
     function echo(string calldata input) public pure returns (string memory) {
         return input;
     }
