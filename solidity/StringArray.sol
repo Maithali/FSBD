901ab39951b0dbac7024b1832d892e3e683b4663
Maithali <ghardem1@gmail.com>
2024-07-05 20:16:41 +0530

    array solidity

--- solidity/StringArray.sol
+++ solidity/StringArray.sol
@@ -2,11 +2,27 @@
 pragma solidity ^0.8.13;
 
 contract Example {
-    function AddressArray(address[] calldata input)public  pure returns (address[] memory) {
+    function AddressArray(
+        address[] calldata input
+    ) public pure returns (address[] memory) {
         return input;
     }
 
-    function StringArray(string[] calldata name) public  pure returns(string[] memory) {
+    function StringArray(
+        string[] calldata name
+    ) public pure returns (string[] memory) {
         return name;
+    }
+
+    function BooleanArray(
+        bool[] calldata value
+    ) public pure returns (bool[] memory) {
+        return value;
+    }
+
+    function IntergerArray(
+        uint256[] calldata no
+    ) public pure returns (uint256[] memory) {
+        return no;
     }
 }
