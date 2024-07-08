7cc70f670e3c29b889a6de05b9674703912d8e2c
Maithali <ghardem1@gmail.com>
2024-07-08 15:40:11 +0530

    solidity learning

--- solidity/StorageVariable.sol
+++ solidity/StorageVariable.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 contract StorageVariable {
-    uint256 internal x;
+    uint256 internal x; //storage variables. They keep their value after the transaction ends
 
     function setX(uint256 newValue) public {
         x = newValue; //set 25
