c4f622676499e49b5d8e7e594ca782923d8439b4
Maithali <ghardem1@gmail.com>
2024-07-08 15:22:47 +0530

    nested array

--- solidity/NestedArray.sol
+++ solidity/NestedArray.sol
@@ -13,7 +13,19 @@ contract NestArray {
             }
         }
         return false;
+    } // [[1,2],[3,4]]true
+    //[[65,78],[33,63]]
+
+    function getRow(
+        uint256[][] calldata nestedArray
+    ) public pure returns (uint256[] memory) {
+        return nestedArray[0]; //[[16,09],[19,97],[45,87]]]
+    }
+
+    //fixedSize
+    function fixedSize(
+        uint256[2][3] calldata nestedfixed
+    ) public pure returns (uint256) {
+        return nestedfixed[2][1]; //[[1,2],[3,4],[5,6]]
     }
 }
-// [[1,2],[3,4]]true
-//[[65,78],[33,63]]
