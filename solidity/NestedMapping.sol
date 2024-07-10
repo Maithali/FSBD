604e6bdb952abe932755ecb4b83449335333157d
Maithali <ghardem1@gmail.com>
2024-07-10 13:21:07 +0530

    solidity learning

---
+++ solidity/NestedMapping.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract ExampleNestedMapping {
+    mapping(uint256 => mapping(uint256 => uint256)) public nestedMapp;
+
+    function setNestedMap(
+        uint256 key1,
+        uint256 key2,
+        uint256 finalvalue
+    ) public {
+        nestedMapp[key1][key2] = finalvalue;
+    }
+}
