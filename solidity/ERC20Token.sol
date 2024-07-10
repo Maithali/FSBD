b04809148830cf108c3b50b70e8a4778f4a956ce
Maithali <ghardem1@gmail.com>
2024-07-10 10:59:57 +0530

    solidity learning

---
+++ solidity/ERC20Token.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract ERC20Token {
+    mapping(address => uint256) public balances; //gives amount of the address
+
+    function setSomeonesBalance(address owner, uint256 amount) public {
+        balances[owner] = amount; // sets address and amount
+    }
+
+    function transferTokenBetweenAddresses(
+        address sender,
+        address receiver,
+        uint256 amount
+    ) public {
+        balances[sender] -= amount; //debit the amount
+        balances[receiver] += amount; //credit the amount
+    }
+}
