00b65d6782482769ec84e99690e6a5ab191fdd0d
Maithali <ghardem1@gmail.com>
2024-07-10 15:41:39 +0530

    solidity learning

---
+++ solidity/specialAddress.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract SpecialAddress {
+    address public banker = 0x617F2E2fD72FD9D5503197092aC168c91465E7f2;
+
+    mapping(address => uint256) public balances;
+
+    function setSomeoneBalance(address owner, uint256 amount) public {
+        if (msg.sender == banker) {
+            balances[owner] = amount;
+        }
+    }
+}
