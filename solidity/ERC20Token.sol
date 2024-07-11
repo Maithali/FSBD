bc65b4339a7bc92511b7c7abfee0f6c799f0174f
Maithali <ghardem1@gmail.com>
2024-07-11 16:30:18 +0530

    solidity learing

--- solidity/ERC20Token.sol
+++ solidity/ERC20Token.sol
@@ -1,21 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.26;
 
-//This implementation has a flaw that anyone can invoke the public functions and send tokens between addresses willy-nilly, but we’ll fix that later.
+// contract ERC20Token {
+//     mapping(address => uint256) public  balances; //gives amount of the address
 
-contract ERC20Token {
-    mapping(address => uint256) public balances; //gives amount of the address
+//     function setSomeonesBalance(address owner, uint256 amount) public  {
+//        balances[owner] = amount; // sets address and amount
+//     }
 
-    function setSomeonesBalance(address owner, uint256 amount) public {
-        balances[owner] = amount; // sets address and amount
-    }
+//     function transferTokenBetweenAddresses(address sender, address receiver, uint256 amount) public  {
+//         balances[sender] -= amount; //debit the amount
+//         balances[receiver] += amount; //credit the amount
+//     }
 
-    function transferTokenBetweenAddresses(
-        address sender,
-        address receiver,
-        uint256 amount
-    ) public {
-        balances[sender] -= amount; //debit the amount
-        balances[receiver] += amount; //credit the amount
-    }
+        contract ERC20 {
+            string public name;
+            string public symbol;
+            
+            constructor(string memory _name, string memory _symbol) {
+
+                name = _name;
+                symbol = _symbol;
+            }
+            
+        }
+    
 }
