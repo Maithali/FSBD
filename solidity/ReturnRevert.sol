fcf35467ce0352b4379879a41c99433fdd1aad01
Maithali <ghardem1@gmail.com>
2024-07-11 14:11:58 +0530

    solidity learing

---
+++ solidity/ReturnRevert.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract ReturnRevert {
+    function mustNotBeFive(uint256 x) public pure returns (uint256) {
+        require(x != 5, "five is not valid");
+        return x * 2;
+    }
+}
