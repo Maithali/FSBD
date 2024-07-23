bf42fed89e0cee84327b320c36b3b768ce419a62
Maithali <ghardem1@gmail.com>
2024-07-23 12:58:50 +0530

    javascript HOF

--- javascript/hof.js
+++ javascript/hof.js
@@ -78,3 +78,8 @@ console.log(e,pi,grav,bodytemp,boiltemp);
 const arr1 = [1,2,3];
 let [var1, , var2] =arr1;
 console.log(var1, var2);
+
+const arr2 = [1, 2, 3, 4, 5, 6, 7, 8];
+let [num1, num2, ...rest] = arr2;
+console.log(num1, num2);
+console.log(rest);
