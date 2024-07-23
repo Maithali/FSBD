0d0b11b73ba814d1911a09dc9f1a5d43a93f18fb
Maithali <ghardem1@gmail.com>
2024-07-23 12:45:16 +0530

    javascript HOF

--- javascript/hof.js
+++ javascript/hof.js
@@ -67,3 +67,14 @@ console.log(sum);
 //sort
 const names = ['anurag','anirudh','hitesh sir','vyom','bishal','mayur','snehal','momin','surya'];
 console.log(names.sort());
+console.log(names.reverse());
+
+
+//destructing
+const sci = [2.72, 3.14, 9.81, 37,100];
+let [e,pi,grav,bodytemp,boiltemp] = sci;
+console.log(e,pi,grav,bodytemp,boiltemp);
+
+const arr1 = [1,2,3];
+let [var1, , var2] =arr1;
+console.log(var1, var2);
