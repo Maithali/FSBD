fb2be02f50796681e812c3151158bbc9d2c00a67
Maithali <ghardem1@gmail.com>
2024-07-24 17:22:55 +0530

    javascript spread

--- javascript/more.js
+++ javascript/more.js
@@ -1,6 +1,22 @@
+
+//Spread
+
 function sumone(x, y) {
     return x + y;
 }
 
 let vari  = [ 9,7,10,11,12,13];
-console.log(sumone(...vari));//spread
+console.log(sumone(...vari));//spread  //16
+
+//Rest
+
+function sum(...args) {
+    console.log(args);
+    let sum = 0;
+    for(const x of args){
+        sum = sum + x;
+    }
+    return sum;
+}
+
+console.log(sum(1,2,3,4,5));
