b93113cc30805afa311d004da7e0802175fab0f5
Maithali <ghardem1@gmail.com>
2024-08-12 08:10:19 +0530

    fundme.sol update

--- solidity/remix-fund-me/FundMe.sol
+++ solidity/remix-fund-me/FundMe.sol
@@ -2,15 +2,20 @@
 pragma solidity ^0.8.18;
 
 contract FundMe {
-    // send funds into our contract
 
+    uint256 public  minimumUsd = 5;
+   
     function fund() public payable {
-        // allow users to send $
-        // have a minimum of $ sent
-        // How do we send ETH to this contract?
-        require(msg.value > 1 ether, "Didn't send enough ETH"); //if the condition is false, revert with the error message
-    }
+    // allow users to send $
+    // have a minimum of $ sent
+    // How do we send ETH to this contract?
+     require(msg.value > 1 ether, "Didn't send enough ETH"); //if the condition is false, revert with the error message
 
+
+}
+   
     // owner can withdraw funds
     /*function withdraw() public {}*/
+
+
 }
