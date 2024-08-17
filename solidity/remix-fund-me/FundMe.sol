c57ea2dd618c597d69eb56a19d90f8291ca664ca
Maithali <ghardem1@gmail.com>
2024-08-17 08:30:41 +0530

    fundMe update.sol

--- solidity/remix-fund-me/FundMe.sol
+++ solidity/remix-fund-me/FundMe.sol
@@ -1,21 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 contract FundMe {
+    uint256 public minimumUsd = 5e18;
 
-    uint256 public  minimumUsd = 5;
-   
     function fund() public payable {
-    // allow users to send $
-    // have a minimum of $ sent
-    // How do we send ETH to this contract?
-     require(msg.value > 1 ether, "Didn't send enough ETH"); //if the condition is false, revert with the error message
+        // allow users to send $
+        // have a minimum of $ sent
+        // How do we send ETH to this contract?
+        require(
+            getConvertionRate(msg.value) >= minimumUsd,
+            "Didn't send enough ETH"
+        ); //if the condition is false, revert with the error message
+    }
 
+    function getPrice() public view returns (uint256) {
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        //ABI
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        //Price of Eth in terms of USD
+        // 2000.00000000
+        return uint256(price * 1e10);
+    }
 
-}
-   
-    // owner can withdraw funds
-    /*function withdraw() public {}*/
+    function getConvertionRate(
+        uint256 ethAmount
+    ) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+    }
 
-
+    function getVersion() public view returns (uint256) {
+        return
+            AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306)
+                .version();
+    }
 }
